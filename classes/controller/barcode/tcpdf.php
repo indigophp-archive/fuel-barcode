fd103c4194d84b2b98199eeb3ba208d6f9c0e502
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-06-24 15:51:23 +0200

    TCPDF barcode controller

--- classes/controller/barcode/tcpdf.php
+++ classes/controller/barcode/tcpdf.php
@@ -4,40 +4,101 @@ namespace Barcode;
 
 class Controller_Barcode_Tcpdf extends \Controller
 {
-	public function action_index($code = null, $type = null, $width = null, $height = null, $color = null)
+
+	protected $barcode;
+
+	protected $data = array(
+		'type'   => 'c39',
+		'ext'    => 'png',
+		'width'  => 2,
+		'height' => 30,
+		'color'  => 'black'
+	);
+
+	public function before($data = null)
 	{
-		is_null($code) && $code = \Input::param('code');
-		is_null($type) && $type = \Input::param('type');
+		parent::before($data);
 
-		is_null($width) && $width = \Input::param('width', \Input::param('w', 2));
-		is_null($height) && $height = \Input::param('height', \Input::param('h', 30));
-		is_null($color) && $color = \Input::param('color', \Input::param('c', 'black'));
+		$this->barcode = Barcode::forge('tcpdf');
 
-		$barcode = Barcode::forge('tcpdf');
-		$barcode->init($code, $type);
+		if ( ! $this->request->is_hmvc())
+		{
+			$data = array(
+				'code'   => \Input::param('code'),
+				'type'   => \Input::param('type', 'c39'),
+				'ext'    => \Input::param('ext', \Input::extension()),
+				'width'  => \Input::param('width', \Input::param('w', 2)),
+				'height' => \Input::param('height', \Input::param('h', 30)),
+				'color'  => \Input::param('color', \Input::param('c', 'black'))
+			);
+			$this->data = \Arr::merge($this->data, $data);
+		}
 
-		switch (\Input::extension()) {
+		$this->data = \Arr::merge($this->data, \Uri::to_assoc(4));
+
+		if ($this->get('ext') == 'png') {
+			$color = \Arr::get($this->data, 'color', '');
+
+			if (is_string($color) && strpos($color, '|') !== false)
+			{
+				\Arr::set($this->data, 'color', explode('|', $color));
+			}
+		}
+
+		$this->barcode->init($this->get('code'), $this->get('type'));
+	}
+
+	public function get($item)
+	{
+		return \Arr::get($this->data, $item);
+	}
+
+	public function action_index()
+	{
+
+		switch ($this->get('ext')) {
 			case 'png':
-				! is_array($color) && $color = array(0, 0, 0);
-				$barcode->getBarcodePNG($width, $height, $color);
+				$this->action_png();
 				break;
 
 			case 'svg':
-				$barcode->getBarcodeSVG($width, $height, $color);
+				$this->action_svg();
 				break;
 
 			case 'svgi':
-				$barcode->getBarcodeSVGcode($width, $height, $color);
+				$this->action_svgi();
 				break;
 
 			case 'html':
-				$barcode->getBarcodeHTML($width, $height, $color);
+				return $this->action_html();
 				break;
 
 			default:
-				! is_array($color) && $color = array(0, 0, 0);
-				$barcode->getBarcodePNG($width, $height, $color);
+				$this->action_png();
 				break;
 		}
+	}
+
+	public function action_png()
+	{
+		$color = $this->get('color');
+		! is_array($color) && $color = array(0, 0, 0);
+
+		$this->barcode->getBarcodePNG($this->get('width'), $this->get('height'), $color);
+	}
+
+	public function action_svg()
+	{
+	$this->barcode->getBarcodeSVG($this->get('width'), $this->get('height'), $this->get('color'));
+	}
+
+	public function action_svgi()
+	{
+		$this->barcode->getBarcodeSVGcode($this->get('width'), $this->get('height'), $this->get('color'));
+	}
+
+	public function action_html()
+	{
+		return $this->barcode->getBarcodeHTML($this->get('width'), $this->get('height'), $this->get('color'));
 	}
 }
